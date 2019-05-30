232fd00172565ddfa7b7f18d575e091d9757066d
Vishal Choudhary <officialbulu@gmail.com>
2019-05-30 13:22:55 +0530

    Created Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Login from './Components/Auth/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     
+      <Login/>
     </div>
   );
 }
